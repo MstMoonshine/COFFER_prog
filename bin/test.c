2e3c506571e83de4862f98e795d8b91cecdb4290
MstMoonshine <11710919@mail.sustech.edu.cn>
2021-09-15 22:03:34 +0800

    feat: test.c for create and run enclave

--- bin/test.c
+++ bin/test.c
@@ -1,6 +1,13 @@
 #include <manager.h>
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	return 0;
+	if (argc != 2) {
+		printf("Usage: %s [path_to_payload]\n", argv[0]);
+		exit(0);
+	}
+
+	unsigned int id = createEnclave(argv[1], -1);
+	enterEnclave(id);
 }
