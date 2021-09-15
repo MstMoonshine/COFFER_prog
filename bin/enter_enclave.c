cfedce4bff09ff8004d7f8ecb9520e61849ae92b
MstMoonshine <11710919@mail.sustech.edu.cn>
2021-09-15 22:05:16 +0800

    fix: include header file in enter_enclave.c

--- bin/enter_enclave.c
+++ bin/enter_enclave.c
@@ -1,4 +1,5 @@
 #include <manager.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,6 @@ int main(int argc, char *argv[])
 
 	unsigned int id = atoi(argv[1]);
 	enterEnclave(id);
-	
+
 	exit(0);
 }
